dd7f4d79c5cad3258131392d488850e4ad422964
Skebede111 <timianbro71@gmail.com>
2026-02-25 17:05:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+messages = []
+
+class Message(BaseModel):
+    username: str
+    text: str
+
 @app.get("/")
 def root():
-    return {"message": "Chat server is working"}
+    return {"status": "Server running"}
+
+@app.get("/messages")
+def get_messages():
+    return messages
+
+@app.post("/send")
+def send_message(msg: Message):
+    messages.append({
+        "username": msg.username,
+        "text": msg.text
+    })
+    return {"status": "Message received"}
